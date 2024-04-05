58dcba4d8e9ce27b031840e6e5deb9f64b145e5a
Imanthi <imanthiabeyratne55@gmail.com>
2024-04-04 18:51:00 -0700

    change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const generateRandomData = require('./dataGenerator');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 const axios = require('axios');
 
 
